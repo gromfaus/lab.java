d0794e9e46fe7f73981863607ef46a16fe852950
KRUK ANDREI <KRUK ANDREI@K4-2-21>
2017-10-02 12:27:29 +0300

    Lab.1

---
+++ Laby/src/com/aleksey/Race.java
@@ -0,0 +1,39 @@
+package com.aleksey;
+
+public class Race {
+	
+	private String date;
+	private int participants;
+	private boolean prize;
+
+	
+	public Race() {}
+	
+	public Race(String date, int participants, boolean prize) {
+		//super();
+		this.date = date;
+		this.participants = participants;
+		this.prize = prize;
+	}
+
+	public String getDate() {
+		return date;
+	}	
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getParticipants() {
+		return participants;
+	}
+	public void setParticipants(int participants) {
+		this.participants = participants;
+	}
+	public boolean hasPrize() {
+		return prize;
+	}
+	public void setPrize(boolean prize) {
+		this.prize = prize;
+	}
+	
+}
